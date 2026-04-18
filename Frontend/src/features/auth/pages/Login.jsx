bbdcaf4e98a4356d12ffacb3dd6bcfaf06c40504
shivam surati <shivamsurati04@gmail.com>
2026-04-19 00:40:09 +0530

    debugging

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, Link } from "react-router";
 import "../auth.form.scss";
+import { useAuth } from "../hooks/useAuth";
 
 const Login = () => {
+  const { loading, handleLogin } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    navigate("/register");
+    // navigate("/register");
+    await handleLogin({ email, password });
   };
+
+  if (loading) {
+    return (
+      <main>
+        <h1>Loading...............</h1>
+      </main>
+    );
+  }
 
   return (
     <main>
@@ -18,6 +33,9 @@ const Login = () => {
           <div className="input-group">
             <label htmlFor="email">Email</label>
             <input
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
               type="email"
               id="email"
               name="email"
@@ -28,6 +46,9 @@ const Login = () => {
           <div className="input-group">
             <label htmlFor="password">Password</label>
             <input
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
               type="password"
               id="password"
               name="password"
--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,16 +1,17 @@
 import axios from "axios";
 
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+  withCredentials: true,
+});
+
 export async function register({ username, email, password }) {
   try {
-    const response = await axios.post(
-      "http://localhost:3000/api/auth/register",
-      {
-        username,
-        email,
-        password,
-      },
-      { withCredentials: true },
-    );
+    const response = await api.post("/api/auth/register", {
+      username,
+      email,
+      password,
+    });
     return response.data;
   } catch (err) {
     console.log(err);
@@ -19,14 +20,10 @@ export async function register({ username, email, password }) {
 
 export async function login({ email, password }) {
   try {
-    const response = await axios.post(
-      "http://localhost:3000/api/auth/login",
-      {
-        email,
-        password,
-      },
-      { withCredentials: true },
-    );
+    const response = await api.post("/api/auth/login", {
+      email,
+      password,
+    });
     return response.data;
   } catch (err) {
     console.log(err);
@@ -35,9 +32,7 @@ export async function login({ email, password }) {
 
 export async function logout() {
   try {
-    const response = await axios.get("http://localhost:3000/api/auth/logout", {
-      withCredentials: true,
-    });
+    const response = await api.get("/api/auth/logout");
     return response.data;
   } catch (err) {
     console.log(err);
@@ -46,9 +41,7 @@ export async function logout() {
 
 export async function getMe() {
   try {
-    const response = await axios.get("http:localhost:3000/api/auth/get-me", {
-      withCredentials: true,
-    });
+    const response = await api.get("/api/auth/get-me");
     return response.data;
   } catch (err) {
     console.log(err);
