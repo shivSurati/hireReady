605d3b5b4fcd57c960d1a37bf2b0e799a927ad2e
shivam surati <shivamsurati04@gmail.com>
2026-04-22 01:50:49 +0530

    added interview.routes.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,6 +15,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes");
+
 app.use("/api/auth", authRouter); //this is called pre-fix      //http://localhost:3000/api/auth/register
+app.use("/api/interview", interviewRouter);
 
 module.exports = app;
---
+++ Backend/src/routes/interview.routes.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const interviewRouter = express.Router();
+
+module.exports = interviewRouter;
