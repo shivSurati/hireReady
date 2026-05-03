6659860e878adf86e3ed1b987d483075bb3d30c8
shivam surati <shivamsurati04@gmail.com>
2026-05-04 01:35:15 +0530

    fix: add vercel url to cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,23 +1,26 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
+    cors({
+        origin: [
+            'http://localhost:5173',
+            'https://hireready-frontend.vercel.app',
+        ],
+        credentials: true,
+    })
 );
 
 app.use(express.json());
 app.use(cookieParser());
 
-const authRouter = require("./routes/auth.routes");
-const interviewRouter = require("./routes/interview.routes");
+const authRouter = require('./routes/auth.routes');
+const interviewRouter = require('./routes/interview.routes');
 
-app.use("/api/auth", authRouter); //this is called pre-fix      //http://localhost:3000/api/auth/register
-app.use("/api/interview", interviewRouter);
+app.use('/api/auth', authRouter); //this is called pre-fix      //http://localhost:3000/api/auth/register
+app.use('/api/interview', interviewRouter);
 
 module.exports = app;
