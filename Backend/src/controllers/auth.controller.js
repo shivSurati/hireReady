9b2b7e06098b94b0e52c048c464eba6067a15579
shivam surati <shivamsurati04@gmail.com>
2026-05-06 00:39:39 +0530

    cookie error fixing

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -42,7 +42,11 @@ async function registerUserController(req, res) {
         { expiresIn: '1d' }
     );
 
-    res.cookie('token', token);
+    res.cookie('token', token, {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+    });
 
     res.status(201).json({
         message: 'User registered successfully',
@@ -84,7 +88,12 @@ async function loginUserController(req, res) {
         { expiresIn: '1d' }
     );
 
-    res.cookie('token', token);
+    res.cookie('token', token, {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+    });
+
     res.status(200).json({
         message: 'User loggedIn successfully.',
         user: {
@@ -107,7 +116,11 @@ async function logoutUserController(req, res) {
         await tokenBlacklistModel.create({ token });
     }
 
-    res.clearCookie('token');
+    res.clearCookie('token', {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+    });
 
     res.status(200).json({
         message: 'User logged out successfully',
